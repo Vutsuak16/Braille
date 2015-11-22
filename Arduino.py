8e213ae4dece60df0ee630ee2be3e71cf3ad9231
kaustuv deolal <vutsuak96@gmail.com>
2015-11-22 16:32:32 +0530

    First Commit

---
+++ Arduino.py
@@ -0,0 +1,24 @@
+__author__ = 'adnan'
+
+import serial
+import time
+f = open("/home/kaustuv/Desktop/text", "r") # change the path here and install pyserial module of python
+string = (f.readline()).strip()
+arduino1 = serial.Serial(port="/dev/ttyUSB1", baudrate=9600)
+for i in string:
+        #time.sleep(2)
+        print i
+        arduino1.write(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
